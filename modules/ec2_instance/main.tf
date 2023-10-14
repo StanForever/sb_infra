7ae2a264c21fbb5af63f0170d79367ca7dc2ded1
Pranit Raje <pranitbraje@gmail.com>
2023-10-14 15:50:21 +0530

    Added EBS encryption

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -4,8 +4,11 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = element(var.public_subnet_ids, count.index)
-
   vpc_security_group_ids = var.security_group_ids
+
+  root_block_device {
+      encrypted = true
+  }
 
   user_data = var.user_data
 
